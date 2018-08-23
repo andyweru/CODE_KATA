3daaf0de1086c530921bc215079ad18171b49765
Andrew Waweru <andyweru@gmail.com>
2018-08-23 22:03:51 +0300

    Which century year belongs to

---
+++ CenturyFromYear.js
@@ -0,0 +1,12 @@
+/*
+
+export const centuryFromYear = (year: number): number => {
+    if(year % 100 == 0){
+      return year / 100;
+    } else {
+      return Math.floor( (year / 100) + 1);
+    }
+};
+
+
+*/
